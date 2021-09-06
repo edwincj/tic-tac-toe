8272be41cefdc09673cd32e44ae2082abd462b22
edwincj <edwincj1998@gmail.com>
2021-09-06 23:11:57 +0530

    CSS,Clicking and some winning logic

--- src/Components/PlayGround.js
+++ src/Components/PlayGround.js
@@ -1,18 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Row from "./Row";
 
 const PlayGround = () => {
   const initial = [
-    ["X", "X", "O"],
-    ["X", "O", "O"],
-    ["X", "O", "X"],
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
   ];
   const [gameState, setGamestate] = useState(initial);
+  const [player, setPlayer] = useState("X");
+
+  const moveHandler = (x, y) => {
+    const newState = [...gameState];
+    newState[x][y] = player;
+    setPlayer((plr) => {
+      if (plr === "X") return "O";
+      return "X";
+    });
+    setGamestate(newState);
+  };
+
+  const checkEachRow = () => {
+    let flg = false;
+    gameState.forEach((row) => {
+      if (row.every((c) => c === "X")) {
+        flg = true;
+        return false;
+      }
+    });
+    return flg;
+  };
+
+  useEffect(() => {
+    let flg = false;
+    gameState.forEach((row) => {
+      if (row.every((c) => c === "X")) {
+        flg = true;
+        return false;
+      }
+    });
+    if (
+      gameState.every((x) => x[0] === "X" || x[1] === "X" || x[2] === "X") ||
+      flg
+    ) {
+      console.log("X");
+    }
+  }, [gameState]);
+
   return (
     <div className="playGround">
       <div className="board">
         {gameState.map((row, index) => {
-          return <Row key={index} value={row} />;
+          return (
+            <Row key={index} rowNum={index} value={row} move={moveHandler} />
+          );
         })}
       </div>
     </div>
--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 const Row = (props) => {
-  const { value } = props;
+  const { rowNum, value, move } = props;
   return (
     <div className="row">
       {value.map((value, index) => {
         return (
-          <div key={index} className="column">
+          <div
+            key={index}
+            className={"column " + (value === "" ? "clickable" : "empty")}
+            onClick={value === "" ? () => move(rowNum, index) : () => {}}
+          >
             {value}
           </div>
         );
